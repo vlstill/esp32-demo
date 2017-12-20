ccb79372b0434ef9c4e6850c0bd5a40725988d45
Vladimir Still <vl.still@gmail.com>
2017-12-20 11:15:10 +0100

    Shift register.

--- src/main.cpp
+++ src/main.cpp
@@ -22,98 +22,86 @@ struct Pin {
     int id = 0;
 };
 
-std::array< Pin, 5 > pins = { 32, 33, 25, 26, 27 };
-std::array< Pin, 2 > ins = { 34, 35 };
+void setup() { }
 
-void set( bool v ) {
-    for ( auto p : pins )
-        p.set( v );
-}
+template< size_t length >
+struct ShiftRegister {
 
-void setup() {
-    std::cout << "init\n";
-    for ( auto p : pins )
-        p.set_output();
-    for ( auto p : ins )
-        p.set_input_pulldown();
+    ShiftRegister( Pin data, Pin clock ) : data( data ), clock( clock )
+    {
+        data.set_output();
+        data.set_low();
+        clock.set_output();
+        clock.set_low();
+        set_all_low();
+    }
 
-    ledcSetup( 0, 20000 /* 20 kHz */, 16 /* 2^16 steps */ );
-    for ( auto p : pins )
-    ledcAttachPin( p.pin(), 0 );
-    ledcWrite( 0, 65536 / 2 ); /* 50 % */
+    void set_all( bool v ) {
+        data.set( v );
+        for ( int i = 0; i < length; ++i ) {
+            clock.set_high();
+            clock.set_low();
+        }
+    }
+
+    void set_all_high() { set_all( true ); }
+    void set_all_low() { set_all( false ); }
+
+    void write() {
+        for ( int i = 0; i < length; ++i ) {
+            data.set( vpins[ length - i - 1 ] );
+            clock.set_high();
+            clock.set_low();
+        }
+    }
+
+    void set( int p, bool v ) {
+        vpins[ p ] = v;
+        write();
+    }
+
+    struct Proxy {
+
+        Proxy( ShiftRegister &sr, int ix ) : _sr( sr ), _ix( ix ) { }
+
+        Proxy &operator=( bool v ) {
+            _sr.set( _ix, v );
+        }
+
+      private:
+        ShiftRegister &_sr;
+        size_t _ix;
+    };
+
+    Proxy operator[]( size_t ix ) {
+        return Proxy( *this, ix );
+    }
+
+    Pin data;
+    Pin clock;
+    std::array< bool, length > vpins;
+};
+
+template< int l >
+void cycle( ShiftRegister< l > &sr ) {
+    for ( ;; ) {
+        sr.set_all_low();
+        delay( 500 );
+        sr.set_all_high();
+        delay( 500 );
+    }
 }
 
 void loop() {
-    const int ANALOG_IN = 36;
-    const int ANALOG_MAX = 4096;
+    ShiftRegister< 8 > sr( 25, 26 );
 
-#if 0
     while ( true ) {
-        int v = analogRead( ANALOG_IN ) * 256 / ANALOG_MAX;
-        ledcWrite( 0, v * v );
-        std::cout << v << std::endl;
-    }
-#endif
-
-    int top = 256;
-    while ( true ) {
-        int d = (analogRead( ANALOG_IN ) * 10000 / ANALOG_MAX) + 1;
-        std::cout << d << std::endl;
-        int v = 0;
-        while ( v <= top ) {
-            int d = (analogRead( ANALOG_IN ) * 10000 / ANALOG_MAX) + 1;
-            ++v;
-            ledcWrite( 0, v * v );
-            delayMicroseconds( d );
-        }
-        while ( v >= 0 ) {
-            int d = (analogRead( ANALOG_IN ) * 10000 / ANALOG_MAX) + 1;
-            ledcWrite( 0, v * v );
-            delayMicroseconds( d );
-            --v;
-        }
-    }
-#if 0
-    int top = 1000;
-    while ( true ) {
-        int v = 0;
-        while ( v <= top ) {
-            ++v;
-            for ( int i = 0; i < top; ++i ) {
-                set( i < v );
-                delayMicroseconds( 1 );
-            }
-        }
-        while ( v >= 0 ) {
-            for ( int i = 0; i < top; ++i ) {
-                set( i < v );
-                delayMicroseconds( 1 );
-            }
-            --v;
-        }
-    }
-#endif
-
-#if 0
-    for ( unsigned i = 0; ; i++ ) {
-        std::cout << "loop " << i << std::endl;
-        for ( int j = 0; j < pins.size(); ++j ) {
-            auto p = pins[j];
-            p.set( i & (1 << j) ? HIGH : LOW );
-        }
-        while ( !ins[0].get() && !ins[1].get() ) {
+        for ( int i = 7; i >= 0; --i ) {
+            sr[ i ] = true;
             delay( 100 );
-        }
-        if ( ins[0].get() ) {
-            while ( ins[0].get() )
-                delay( 100 );
-            continue;
-        }
-        if ( ins[1].get() ) {
-            while ( ins[1].get() )
-                delay( 100 );
-            i = -1;
+            sr[ i ] = false;
         }
     }
-#endif
+
+    while ( true ) { }
 }
